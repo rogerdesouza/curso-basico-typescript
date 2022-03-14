16a97034d4b0aeedf61bc7d3b0b4c58603fae489
roger <roger@policiacivil.pe.gov.br>
2022-03-14 14:25:04 -0300

    ajustes

---
+++ src/04-poo/01-classes.ts
@@ -0,0 +1,36 @@
+export class Empresa {
+  public readonly nome: string;
+  private readonly colaboradores: Colaborador[] = [];
+  protected readonly cnpj: string;
+
+  constructor(nome: string, cnpj: string) {
+    this.nome = nome;
+    this.cnpj = cnpj;
+  }
+
+  adicionaColaborador(colaborador: Colaborador): void {
+    this.colaboradores.push(colaborador);
+  }
+
+  mostrarColaboradores(): void {
+    for (const colaborador of this.colaboradores) {
+      console.log(colaborador);
+    }
+  }
+}
+
+export class Colaborador {
+  constructor(public readonly nome: string) {}
+}
+
+const empresa1 = new Empresa('Udemy', '11.111.111/0001-11');
+
+const colaborador1 = new Colaborador('Roger');
+const colaborador2 = new Colaborador('Vanessa');
+const colaborador3 = new Colaborador('Vinicius');
+empresa1.adicionaColaborador(colaborador1);
+empresa1.adicionaColaborador(colaborador2);
+empresa1.adicionaColaborador(colaborador3);
+
+console.log(empresa1);
+empresa1.mostrarColaboradores();
