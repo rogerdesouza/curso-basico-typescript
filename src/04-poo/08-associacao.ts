daca51233c1441cbfb0110947d3e46ae56406248
roger <roger@policiacivil.pe.gov.br>
2022-03-30 06:55:06 -0300

    ajustes

--- src/04-poo/08-associacao.ts
+++ src/04-poo/08-associacao.ts
@@ -1,20 +1,36 @@
-export abstract class Escritor {
+export class Escritor {
   private _ferramenta: Ferramenta | null = null;
   constructor(private _nome: string) {}
 
   get nome(): string {
     return this.nome;
   }
+
+  set ferramenta(ferramenta: Ferramenta) {
+    this._ferramenta = ferramenta;
+  }
 }
 
 export abstract class Ferramenta {
   constructor(private _nome: string) {}
 
-  getNome(): string {
-    return this.nome;
+  get nome(): string {
+    return this._nome;
   }
 
   abstract escrever(): void;
+}
+
+export class Caneta extends Ferramenta {
+  escrever(): void {
+    console.log(`Estou escrevendo com a caneta ${this.nome}`);
+  }
+}
+
+export class MaquinaEscrever extends Ferramenta {
+  escrever(): void {
+    console.log(`Estou digitando com a máquina de escrever ${this.nome}`);
+  }
 }
 
 // export class Caneta extends Ferramenta {
@@ -28,17 +44,3 @@ export abstract class Ferramenta {
 // }
 
 // const caneta = new Caneta('Bic');
-
-
-
-22
-
-2345567566666
-
-
-
-123456789/+-*XPathExpression
-=
-
-
-==
